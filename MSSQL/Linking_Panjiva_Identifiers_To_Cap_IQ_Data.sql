ed51fe7cd75df99ca51234925d050b310f5ff233
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-11-26 20:00:08 +0530

    Update Linking_Panjiva_Identifiers_To_Cap_IQ_Data.sql

--- MSSQL/Linking_Panjiva_Identifiers_To_Cap_IQ_Data.sql
+++ MSSQL/Linking_Panjiva_Identifiers_To_Cap_IQ_Data.sql
@@ -22,27 +22,27 @@ Query_Added_Date:
 DatasetKey:
 22
 
-The following sample query links Panjiva identifiers to S&P Capital IQ data.Note: Here TOP 100 are shown for better query execution
+The following sample query links Panjiva identifiers to S&P Capital IQ data.
+Note:Here TOP 100 are shown for better query execution
 
 ***********************************************************************************************/
-
 select Top 100 panjivaRecordId, shpPanjivaId, csh.company, csh.ciqID, conPanjivaId, cco.company, cco.ciqID
 from panjivaUSImport2019 im
 left join (
- SELECT c.companyID as ciqID, c.companyName as company, ccr.identifiervalue
- FROM xfl_ciq.dbo.ciqCompany c
- JOIN xfl_ciq.dbo.ciqCompanyUltimateParent cup
- ON cup.ultimateParentCompanyId = c.companyId
- JOIN xfl_panjiva.dbo.panjivaCompanyCrossRef ccr
- ON ccr.companyId = cup.companyId
- GROUP BY c.companyId, c.companyName, ccr.identifiervalue
- ) csh ON im.shpPanjivaId = csh.identifiervalue
+SELECT c.companyID as ciqID, c.companyName as company, ccr.identifiervalue
+FROM xfl_ciq.dbo.ciqCompany c
+JOIN xfl_ciq.dbo.ciqCompanyUltimateParent cup
+ON cup.ultimateParentCompanyId = c.companyId
+JOIN xfl_panjiva.dbo.panjivaCompanyCrossRef ccr
+ON ccr.companyId = cup.companyId
+GROUP BY c.companyId, c.companyName, ccr.identifiervalue
+) csh ON im.shpPanjivaId = csh.identifiervalue
 left join (
- SELECT c.companyID as ciqID, c.companyName as company, ccr.identifiervalue
- FROM xfl_ciq.dbo.ciqCompany c
- JOIN xfl_ciq.dbo.ciqCompanyUltimateParent cup
- ON cup.ultimateParentCompanyId = c.companyId
- JOIN xfl_panjiva.dbo.panjivaCompanyCrossRef ccr
- ON ccr.companyId = cup.companyId
- GROUP BY c.companyId, c.companyName, ccr.identifiervalue
- ) cco ON im.shpPanjivaId = cco.identifiervalue
+SELECT c.companyID as ciqID, c.companyName as company, ccr.identifiervalue
+FROM xfl_ciq.dbo.ciqCompany c
+JOIN xfl_ciq.dbo.ciqCompanyUltimateParent cup
+ON cup.ultimateParentCompanyId = c.companyId
+JOIN xfl_panjiva.dbo.panjivaCompanyCrossRef ccr
+ON ccr.companyId = cup.companyId
+GROUP BY c.companyId, c.companyName, ccr.identifiervalue
+) cco ON im.shpPanjivaId = cco.identifiervalue
